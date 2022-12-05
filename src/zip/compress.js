e45e5263e4ed773e46a63eecfb0c4d6a9352251e
Mikhail Mudrogelov <mudrogelov.beled@gmail.com>
2022-12-06 03:30:54 +0400

    feat: implement zip/compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const srcPath = './src/zip/files/fileToCompress.txt';
+    const targetPath = './src/zip/files/archive.gz';
+
+    const handleStream = createReadStream(srcPath)
+    handleStream
+        .pipe(createGzip())
+        .pipe(createWriteStream(targetPath))
 };
 
 await compress();
