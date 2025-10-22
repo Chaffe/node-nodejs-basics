4372182f3ed34d3bab111efd53f4e53ee418ba6a
Mikhail <mikhail.mudrogelov@loquatinc.io>
2025-10-23 01:01:33 +0300

    feat: implement fs-module

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { access, rm } from "node:fs/promises";
+
 const remove = async () => {
-  // Write your code here
+    const PATHNAME = "./src/fs/files/fileToRemove.txt";
+    const NON_EXIST_FILE_ERROR = "\x1b[31m"+ "FS operation failed" + "\x1b[0m";
+
+    try {
+        await access(PATHNAME);
+        await rm(PATHNAME);
+    } catch (err) {
+        if (err.code === "ENOENT") {
+            throw new Error(NON_EXIST_FILE_ERROR);
+        }
+    }
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import { rename as fsRename, access } from "node:fs/promises";
+
 const rename = async () => {
-  // Write your code here
+    const OLD_PATHNAME = "./src/fs/files/wrongFilename.txt";
+    const NEW_PATHNAME = "./src/fs/files/properFilename.md";
+    const NON_EXIST_FILE_ERROR = "\x1b[31m"+ "FS operation failed" + "\x1b[0m";
+
+    try {
+        await access(OLD_PATHNAME);
+        await fsRename(OLD_PATHNAME, NEW_PATHNAME);
+    } catch (err) {
+        if (err.code === "ENOENT" || err.message === NON_EXIST_FILE_ERROR) {
+            throw new Error(NON_EXIST_FILE_ERROR);
+        }
+    }
 };
 
 await rename();
