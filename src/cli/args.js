1e5c197fa131b63e3aa66bdd82fcbd901dd91881
Mikhail <mikhail.mudrogelov@gmail.com>
2025-10-23 12:04:07 +0300

    feat: implement cli-module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-  // Write your code here
+  const filteredArgs = argv.slice(2);
+  const iterationSize = 2;
+
+  const result = [];
+  for (let i = 0; i < filteredArgs.length; i += iterationSize) {
+    result.push(`${filteredArgs[i].slice(2)} is ${filteredArgs[i + 1]}`);
+  }
+
+  console.log(result.join(", "));
 };
 
 parseArgs();
