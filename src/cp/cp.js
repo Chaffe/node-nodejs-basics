ec7ec3e5174251983a46ed8bd8ca738559cb7f2f
Mikhail <mikhail.mudrogelov@gmail.com>
2025-10-26 23:44:02 +0300

    feat: implement cp- & modules-tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const MODULE_PATH = "./src/cp/files/script.js";
+
+  const child = fork(MODULE_PATH, args, { silent: true });
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['123', '1', 1, true, null]);
