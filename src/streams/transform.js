aecb880f89add61a7055932d5ce77d72f657a8f6
Mikhail <mikhail.mudrogelov@gmail.com>
2025-10-27 02:24:05 +0300

    feat: implement steams-transform and worker tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-  // Write your code here
+  const reversedTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      const chunkValue = chunk.toString().trim();
+      const reversedChunkValue = chunkValue.split('').reverse().join('');
+      this.push(reversedChunkValue + '\n');
+      callback();
+    },
+  });
+  pipeline(process.stdin, reversedTransform, process.stdout);
 };
 
 await transform();
